5d68a8b53621114637c4d9ea356c6cb5f4e3b719
Ilie Robert Stefan <78737658+Robicu22@users.noreply.github.com>
2025-11-08 15:34:03 +0200

    mesaj

--- backend/index.js
+++ backend/index.js
@@ -1,27 +1,29 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import cors from 'cors';
 import { connectDB } from './config/db.js';
 import dotenv from 'dotenv';
 import userRoutes from './routes/userRoutes.js';
+import cors from 'cors';
+
+app.use(cors({
+  origin: 'http://localhost:5173',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}));
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Enable CORS for frontend
-app.use(cors({
-  origin: 'http://localhost:5173',
-  credentials: true
-}));
-
-app.use(bodyParser.json());
-
-app.use("/api", userRoutes);
-
-connectDB();
-
 app.listen(PORT, () => {
   console.log(`Server is running on port: ${PORT}`);
 });
+
+connectDB();
+
+
+app.use(bodyParser.json());
+
+app.use("/api",userRoutes);
+
