c7cb4a83e875cb50164690fadbee565a0b86fc61
Costijk <119739051+Costijk@users.noreply.github.com>
2025-11-08 09:29:12 +0200

    frontend

---
+++ frontend/src/main.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import PageExample from "./pages/PageExample";
+import App from "./App";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/example",
+    element: <PageExample />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
---
+++ frontend/src/pages/PageExample.jsx
@@ -0,0 +1,7 @@
+export default function PageExample() {
+  return (
+    <div>
+      <h1>Example Page</h1>
+    </div>
+  );
+}
